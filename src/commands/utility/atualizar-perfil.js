12f50e65e00cdf4ac8b9ffa755e349bf00a23fc9
Alan Santos Coutinho <alanscoutinho0@gmail.com>
2025-11-04 16:50:34 +0000

    Como já discutimos: /iniciar perfil não existe mais e nem /atualizar per

--- src/commands/utility/atualizar-perfil.js
+++ src/commands/utility/atualizar-perfil.js
@@ -1,3 +1,3 @@
 // src/commands/utility/atualizar-perfil.js
 // Este comando foi desativado, pois a funcionalidade de canais de perfil foi removida.
-// A atualização de perfil agora é feita através do comando /iniciar-perfil.
+// A atualização de perfil agora é feita através do comando /perfil.
