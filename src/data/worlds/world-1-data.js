2aa3c235cb5bc845114e2f8a335d581f6dad52bb
Alan Santos Coutinho <alanscoutinho0@gmail.com>
2025-11-07 16:40:26 +0000

    Corrija. O bônus é um para cada, energia e outro dano. Não é dois bônus

--- src/data/worlds/world-1-data.js
+++ src/data/worlds/world-1-data.js
@@ -123,25 +123,25 @@ export const world1Data = {
         achievements: {
             headers: ['Conquista', 'Requisito', 'Bônus'],
             rows: [
-                { 'Conquista': 'Halloween Raid I', 'Requisito': 'Reach Wave 50', 'Bônus': '1% Dano' },
-                { 'Conquista': 'Halloween Raid II', 'Requisito': 'Reach Wave 100', 'Bônus': '1% Energia' },
-                { 'Conquista': 'Halloween Raid III', 'Requisito': 'Reach Wave 150', 'Bônus': '2% Dano' },
-                { 'Conquista': 'Halloween Raid IV', 'Requisito': 'Reach Wave 200', 'Bônus': '2% Energia' },
-                { 'Conquista': 'Halloween Raid V', 'Requisito': 'Reach Wave 250', 'Bônus': '3% Dano' },
-                { 'Conquista': 'Halloween Raid VI', 'Requisito': 'Reach Wave 300', 'Bônus': '3% Energia' },
-                { 'Conquista': 'Halloween Raid VII', 'Requisito': 'Reach Wave 350', 'Bônus': '4% Dano' },
-                { 'Conquista': 'Halloween Raid VIII', 'Requisito': 'Reach Wave 400', 'Bônus': '4% Energia' },
-                { 'Conquista': 'Halloween Raid IX', 'Requisito': 'Reach Wave 450', 'Bônus': '5% Dano' },
-                { 'Conquista': 'Halloween Raid X', 'Requisito': 'Reach Wave 500', 'Bônus': '5% Energia' },
-                { 'Conquista': 'Halloween Raid XI', 'Requisito': 'Reach Wave 550', 'Bônus': '6% Dano' },
-                { 'Conquista': 'Halloween Raid XII', 'Requisito': 'Reach Wave 600', 'Bônus': '6% Energia' },
-                { 'Conquista': 'Halloween Raid XIII', 'Requisito': 'Reach Wave 650', 'Bônus': '7% Dano' },
-                { 'Conquista': 'Halloween Raid XIV', 'Requisito': 'Reach Wave 700', 'Bônus': '7% Energia' },
-                { 'Conquista': 'Halloween Raid XV', 'Requisito': 'Reach Wave 750', 'Bônus': '8% Dano' },
-                { 'Conquista': 'Halloween Raid XVI', 'Requisito': 'Reach Wave 800', 'Bônus': '8% Energia' },
-                { 'Conquista': 'Halloween Raid XVII', 'Requisito': 'Reach Wave 850', 'Bônus': '9% Dano' },
-                { 'Conquista': 'Halloween Raid XVIII', 'Requisito': 'Reach Wave 900', 'Bônus': '9% Energia' },
-                { 'Conquista': 'Halloween Raid XIX', 'Requisito': 'Reach Wave 950', 'Bônus': '10% Dano' },
+                { 'Conquista': 'Halloween Raid I', 'Requisito': 'Reach Wave 50', 'Bônus': '1% Energia' },
+                { 'Conquista': 'Halloween Raid II', 'Requisito': 'Reach Wave 100', 'Bônus': '2% Dano' },
+                { 'Conquista': 'Halloween Raid III', 'Requisito': 'Reach Wave 150', 'Bônus': '3% Energia' },
+                { 'Conquista': 'Halloween Raid IV', 'Requisito': 'Reach Wave 200', 'Bônus': '4% Dano' },
+                { 'Conquista': 'Halloween Raid V', 'Requisito': 'Reach Wave 250', 'Bônus': '5% Energia' },
+                { 'Conquista': 'Halloween Raid VI', 'Requisito': 'Reach Wave 300', 'Bônus': '6% Dano' },
+                { 'Conquista': 'Halloween Raid VII', 'Requisito': 'Reach Wave 350', 'Bônus': '7% Energia' },
+                { 'Conquista': 'Halloween Raid VIII', 'Requisito': 'Reach Wave 400', 'Bônus': '8% Dano' },
+                { 'Conquista': 'Halloween Raid IX', 'Requisito': 'Reach Wave 450', 'Bônus': '9% Energia' },
+                { 'Conquista': 'Halloween Raid X', 'Requisito': 'Reach Wave 500', 'Bônus': '10% Dano' },
+                { 'Conquista': 'Halloween Raid XI', 'Requisito': 'Reach Wave 550', 'Bônus': '11% Energia' },
+                { 'Conquista': 'Halloween Raid XII', 'Requisito': 'Reach Wave 600', 'Bônus': '12% Dano' },
+                { 'Conquista': 'Halloween Raid XIII', 'Requisito': 'Reach Wave 650', 'Bônus': '13% Energia' },
+                { 'Conquista': 'Halloween Raid XIV', 'Requisito': 'Reach Wave 700', 'Bônus': '14% Dano' },
+                { 'Conquista': 'Halloween Raid XV', 'Requisito': 'Reach Wave 750', 'Bônus': '15% Energia' },
+                { 'Conquista': 'Halloween Raid XVI', 'Requisito': 'Reach Wave 800', 'Bônus': '16% Dano' },
+                { 'Conquista': 'Halloween Raid XVII', 'Requisito': 'Reach Wave 850', 'Bônus': '17% Energia' },
+                { 'Conquista': 'Halloween Raid XVIII', 'Requisito': 'Reach Wave 900', 'Bônus': '18% Dano' },
+                { 'Conquista': 'Halloween Raid XIX', 'Requisito': 'Reach Wave 950', 'Bônus': '19% Energia' },
                 { 'Conquista': 'Halloween Raid XX', 'Requisito': 'Reach Wave 1000', 'Bônus': 'Halloween Bag' },
             ]
         }
@@ -153,5 +153,3 @@ export const world1Data = {
       }
   ]
 };
-
-    
