6c4ad4c0d8daf547b1b6db9a51f6933da065b7f3
Alan Santos Coutinho <alanscoutinho0@gmail.com>
2025-11-07 21:03:56 +0000

    A informação sobre ter uma parte da Hero license f no mundo 3 pode deixa

--- src/data/worlds/world-3-data.js
+++ src/data/worlds/world-3-data.js
@@ -8,6 +8,7 @@ export const world3Data = {
     { name: 'Ichige', rank: 'D', exp: 22, hp: '2.5qd', drops: { coins: { amount: 'x10k', probability: 1 }, 'Reiatsu Token': { amount: 'x1-5', probability: 0.1 }, 'Pressure Token': { amount: 'x1-5', probability: 0.1 }, exp: { amount: 'x22', probability: 1 }, 'Avatar Soul': { amount: 'x1', probability: 0.11 }, weapon: { name: 'Zangetsu', probability: 0.25 } } },
     { name: 'Uryua', rank: 'C', exp: 23, hp: '55qd', drops: { coins: { amount: 'x15k', probability: 1 }, 'Reiatsu Token': { amount: 'x1-5', probability: 0.1 }, 'Pressure Token': { amount: 'x1-5', probability: 0.1 }, exp: { amount: 'x23', probability: 1 }, 'Avatar Soul': { amount: 'x1', probability: 0.125 } } },
     { name: 'Rakiu', rank: 'B', exp: 24, hp: '160qd', drops: { coins: { amount: 'x20k', probability: 1 }, 'Reiatsu Token': { amount: 'x1-5', probability: 0.1 }, 'Pressure Token': { amount: 'x1-5', probability: 0.1 }, exp: { amount: 'x24', probability: 1 }, 'Avatar Soul': { amount: 'x1', probability: 0.15 } } },
+    { name: 'Yoichi', rank: 'A', exp: 25, hp: '450qd', drops: { coins: { amount: 'x25k', probability: 1 }, 'Reiatsu Token': { amount: 'x1-5', probability: 0.1 }, 'Pressure Token': { amount: 'x1-5', probability: 0.1 }, exp: { amount: 'x25', probability: 1 }, 'Avatar Soul': { amount: 'x1', probability: 0.2 } } },
     { name: 'Kahara', rank: 'S', exp: 26, hp: '1Qn', drops: { coins: { amount: 'x30k', probability: 1 }, 'Reiatsu Token': { amount: 'x1-5', probability: 0.1 }, 'Pressure Token': { amount: 'x1-5', probability: 0.1 }, exp: { amount: 'x26', probability: 1 }, 'Avatar Soul': { amount: 'x1', probability: 0.25 } } },
     { name: 'Eizen', rank: 'SS', exp: 60, hp: '2.5Sp', drops: { coins: { amount: 'x70k', probability: 1 }, 'Zanpakuto Token': { amount: 'x3-5', probability: 0.1 }, 'Reiatsu Token': { amount: 'x3-5', probability: 0.1 }, 'Pressure Token': { amount: 'x3-5', probability: 0.1 }, exp: { amount: 'x60', probability: 1 }, 'Avatar Soul': { amount: 'x1', probability: 0.5 }, aura: { name: 'Purple Traitor Aura', probability: 0.01 } }, videoUrl: 'https://cdn.discordapp.com/attachments/1430337273794265250/1430337718872965202/ScreenRecording_10-21-2025_10-30-50_1.mov?ex=68fa120e&is=68f8c08e&hm=8f03a2541a21bf69091297a2f4aa56de4f9b8615118efcb767705ff29e4fc60f&' },
   ],
@@ -15,8 +16,8 @@ export const world3Data = {
     { name: 'Hime', rarity: 'Comum', energy_bonus: '19' },
     { name: 'Ichige', rarity: 'Incomum', energy_bonus: '38' },
     { name: 'Uryua', rarity: 'Raro', energy_bonus: '56' },
-    { name: 'Rakiu', rarity: 'Épico', energy_bonus: '75' },
-    { name: 'Yoichi', rarity: 'Lendário', energy_bonus: '94' },
+    { name: 'Rakiu', rarity: 'Raro', energy_bonus: '75' },
+    { name: 'Yoichi', rarity: 'Épico', energy_bonus: '94' },
     { name: 'Kahara', rarity: 'Mítico', energy_bonus: '125' },
     { name: 'Eizen', rarity: 'Phantom', energy_bonus: '281' },
   ],
@@ -44,61 +45,5 @@ export const world3Data = {
   ],
   dungeons: [
       { name: 'Las Noches', boss: 'Ulquiorra', description: 'O palácio dos Arrancars em Hueco Mundo.'}
-  ],
-  missions: [
-    {
-        name: 'Missão #1',
-        requirement: 'Derrotar 30 Hollows',
-        rewards: [
-            { name: 'World Key', amount: 1 },
-            { name: 'Exp', amount: '2k' }
-        ]
-    },
-    {
-        name: 'Missão #2',
-        requirement: 'Derrotar 25 Shinigami Novato',
-        rewards: [
-            { name: 'World Key', amount: 1 },
-            { name: 'Shinigami Power Token', amount: 10 },
-            { name: 'Exp', amount: '3k' }
-        ]
-    },
-    {
-        name: 'Missão #3',
-        requirement: 'Derrotar 20 Arrancar',
-        rewards: [
-            { name: 'World Key', amount: 1 },
-            { name: 'Drop Percent', amount: '2%' },
-            { name: 'Exp', amount: '4.5k' }
-        ]
-    },
-    {
-        name: 'Missão #4',
-        requirement: 'Derrotar 15 Tenente Shinigami',
-        rewards: [
-            { name: 'World Key', amount: 1 },
-            { name: 'Avatar Soul', amount: 20 },
-            { name: 'Exp', amount: '6k' }
-        ]
-    },
-    {
-        name: 'Missão #5',
-        requirement: 'Derrotar 10 Capitão Shinigami',
-        rewards: [
-            { name: 'World Key', amount: 1 },
-            { name: 'Shinigami Power Token', amount: 20 },
-            { name: 'Exp', amount: '8k' }
-        ]
-    },
-    {
-        name: 'Missão #6',
-        requirement: 'Derrotar 5 Ichigo',
-        rewards: [
-            { name: 'World Key', amount: 1 },
-            { name: 'Pet Chest', amount: 1 },
-            { name: 'Joia', amount: 1 },
-            { name: 'Exp', amount: '10k' }
-        ]
-    }
   ]
 };
