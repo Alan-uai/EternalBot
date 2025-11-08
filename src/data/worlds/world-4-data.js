ce652779e19e9578c8d33d1d22a9e08d9b90dbbd
Alan Santos Coutinho <alanscoutinho0@gmail.com>
2025-11-08 00:56:52 +0000

    Perfeito só faça uma pequena atualização: no nome do token, Cursed Finge

--- src/data/worlds/world-4-data.js
+++ src/data/worlds/world-4-data.js
@@ -5,13 +5,13 @@ export const world4Data = {
   summary: 'Um mundo infernal focado em dano e drops, com o chefe Sakuni.',
   content: 'Mundo temático de Shinigamis. É aqui que os jogadores encontram a primeira espada de energia do jogo, a Zangetsu.',
   npcs: [
-    { name: 'Itodo', rank: 'E', exp: 39, hp: '1.5Qn', drops: { coins: { amount: 'x50k', probability: 1 }, 'Cursed Token': { amount: 'x1-5', probability: 0.1 }, 'Cursed Finger': { amount: 'x1-5', probability: 0.1 }, 'Avatar Soul': { amount: 'x1', probability: 0.1 } } },
-    { name: 'Nebara', rank: 'D', exp: 41, hp: '50Qn', drops: { coins: { amount: 'x100k', probability: 1 }, 'Cursed Token': { amount: 'x1-5', probability: 0.1 }, 'Cursed Finger': { amount: 'x1-5', probability: 0.1 }, 'Avatar Soul': { amount: 'x1', probability: 0.11 } } },
-    { name: 'Magum', rank: 'C', exp: 43, hp: '110Qn', drops: { coins: { amount: 'x150k', probability: 1 }, 'Cursed Token': { amount: 'x1-5', probability: 0.1 }, 'Cursed Finger': { amount: 'x1-5', probability: 0.1 }, 'Avatar Soul': { amount: 'x1', probability: 0.125 } } },
-    { name: 'Meki', rank: 'B', exp: 45, hp: '475Qn', drops: { coins: { amount: 'x200k', probability: 1 }, 'Cursed Token': { amount: 'x1-5', probability: 0.1 }, 'Cursed Finger': { amount: 'x1-5', probability: 0.1 }, 'Avatar Soul': { amount: 'x1', probability: 0.15 } } },
-    { name: 'Tage', rank: 'A', exp: 47, hp: '9.69sx', drops: { coins: { amount: 'x250k', probability: 1 }, 'Cursed Token': { amount: 'x1-5', probability: 0.1 }, 'Cursed Finger': { amount: 'x1-5', probability: 0.1 }, 'Avatar Soul': { amount: 'x1', probability: 0.2 } } },
-    { name: 'Gajo', rank: 'S', exp: 50, hp: '50sx', drops: { coins: { amount: 'x300k', probability: 1 }, 'Cursed Token': { amount: 'x1-5', probability: 0.1 }, 'Cursed Finger': { amount: 'x1-5', probability: 0.1 }, 'Avatar Soul': { amount: 'x1', probability: 0.25 } } },
-    { name: 'Sakuni', rank: 'SS', exp: 120, hp: '120Sp', drops: { coins: { amount: 'x700k', probability: 1 }, 'Cursed Token': { amount: 'x3-5', probability: 0.1 }, 'Cursed Finger': { amount: 'x3-5', probability: 0.1 }, 'Avatar Soul': { amount: 'x1', probability: 0.5 }, aura: { name: 'Fire King Aura', probability: 0.01 } }, videoUrl: 'https://cdn.discordapp.com/attachments/1430337273794265250/1430337777840554126/ScreenRecording_10-21-2025_10-31-17_1.mov?ex=68fa121c&is=68f8c09c&hm=4701c56efe6c8b63f39d235ca2353f417c474b290038f6c2874b44537ce6ead1&' },
+    { name: 'Itodo', rank: 'E', exp: 39, hp: '1.5Qn', drops: { coins: { amount: 'x50k', probability: 1 }, 'Cursed Token': { amount: 'x1-5', probability: 0.1 }, 'Cursed Finger Token': { amount: 'x1-5', probability: 0.1 }, 'Avatar Soul': { amount: 'x1', probability: 0.1 } } },
+    { name: 'Nebara', rank: 'D', exp: 41, hp: '50Qn', drops: { coins: { amount: 'x100k', probability: 1 }, 'Cursed Token': { amount: 'x1-5', probability: 0.1 }, 'Cursed Finger Token': { amount: 'x1-5', probability: 0.1 }, 'Avatar Soul': { amount: 'x1', probability: 0.11 } } },
+    { name: 'Magum', rank: 'C', exp: 43, hp: '110Qn', drops: { coins: { amount: 'x150k', probability: 1 }, 'Cursed Token': { amount: 'x1-5', probability: 0.1 }, 'Cursed Finger Token': { amount: 'x1-5', probability: 0.1 }, 'Avatar Soul': { amount: 'x1', probability: 0.125 } } },
+    { name: 'Meki', rank: 'B', exp: 45, hp: '475Qn', drops: { coins: { amount: 'x200k', probability: 1 }, 'Cursed Token': { amount: 'x1-5', probability: 0.1 }, 'Cursed Finger Token': { amount: 'x1-5', probability: 0.1 }, 'Avatar Soul': { amount: 'x1', probability: 0.15 } } },
+    { name: 'Tage', rank: 'A', exp: 47, hp: '9.69sx', drops: { coins: { amount: 'x250k', probability: 1 }, 'Cursed Token': { amount: 'x1-5', probability: 0.1 }, 'Cursed Finger Token': { amount: 'x1-5', probability: 0.1 }, 'Avatar Soul': { amount: 'x1', probability: 0.2 } } },
+    { name: 'Gajo', rank: 'S', exp: 50, hp: '50sx', drops: { coins: { amount: 'x300k', probability: 1 }, 'Cursed Token': { amount: 'x1-5', probability: 0.1 }, 'Cursed Finger Token': { amount: 'x1-5', probability: 0.1 }, 'Avatar Soul': { amount: 'x1', probability: 0.25 } } },
+    { name: 'Sakuni', rank: 'SS', exp: 120, hp: '120Sp', drops: { coins: { amount: 'x700k', probability: 1 }, 'Cursed Token': { amount: 'x3-5', probability: 0.1 }, 'Cursed Finger Token': { amount: 'x3-5', probability: 0.1 }, 'Avatar Soul': { amount: 'x1', probability: 0.5 }, aura: { name: 'Fire King Aura', probability: 0.01 } }, videoUrl: 'https://cdn.discordapp.com/attachments/1430337273794265250/1430337777840554126/ScreenRecording_10-21-2025_10-31-17_1.mov?ex=68fa121c&is=68f8c09c&hm=4701c56efe6c8b63f39d235ca2353f417c474b290038f6c2874b44537ce6ead1&' },
   ],
   pets: [
     { name: 'Itodo', rarity: 'Comum', energy_bonus: '47' },
@@ -46,13 +46,13 @@ export const world4Data = {
       unlockCost: "Varia",
       description: "Poder de dano e crítico de energia obtido na Cursed Raid.",
       stats: [
-        { name: 'Common Curse', statType: 'damage', multiplier: '0.6x', rarity: 'Comum' },
-        { name: 'Uncommon Curse', statType: 'damage', multiplier: '0.8x', rarity: 'Incomum' },
-        { name: 'Rare Curse', statType: 'damage', multiplier: '1x', rarity: 'Raro', energy_crit_bonus: '1.00%' },
-        { name: 'Epic Curse', statType: 'damage', multiplier: '2x', rarity: 'Épico', energy_crit_bonus: '2.00%' },
-        { name: 'Legendary Curse', statType: 'damage', multiplier: '3x', rarity: 'Lendário', energy_crit_bonus: '3.00%' },
-        { name: 'Mythical Curse', statType: 'damage', multiplier: '4x', rarity: 'Mítico', energy_crit_bonus: '4.00%' },
-        { name: 'Phantom Curse', statType: 'damage', multiplier: '5x', rarity: 'Phantom', energy_crit_bonus: '5.00%' }
+        { name: 'Common Curse', statType: 'damage', multiplier: '0.6x', rarity: 'Comum', probability: 40.55 },
+        { name: 'Uncommon Curse', statType: 'damage', multiplier: '0.8x', rarity: 'Incomum', probability: 33 },
+        { name: 'Rare Curse', statType: 'damage', multiplier: '1x', rarity: 'Raro', energy_crit_bonus: '1.00%', probability: 19.9 },
+        { name: 'Epic Curse', statType: 'damage', multiplier: '2x', rarity: 'Épico', energy_crit_bonus: '2.00%', probability: 5 },
+        { name: 'Legendary Curse', statType: 'damage', multiplier: '3x', rarity: 'Lendário', energy_crit_bonus: '3.00%', probability: 1 },
+        { name: 'Mythical Curse', statType: 'damage', multiplier: '4x', rarity: 'Mítico', energy_crit_bonus: '4.00%', probability: 0.5 },
+        { name: 'Phantom Curse', statType: 'damage', multiplier: '5x', rarity: 'Phantom', energy_crit_bonus: '5.00%', probability: 0.05 }
       ]
     },
     {
@@ -68,7 +68,55 @@ export const world4Data = {
       { id: 'chifres-demonio', name: 'Chifres de Demônio', world: 'Mundo 4', boss: 'Lorde Demônio', rarity: 'Épico', damage_bonus: '0.05x' }
   ],
   dungeons: [
-      { name: 'Cursed Raid', boss: 'Desconhecido', description: 'Uma raid onde se obtém os poderes Curses e Cursed Power.'}
+      { 
+        name: 'Cursed Raid', 
+        boss: 'Desconhecido', 
+        description: 'Uma raid onde se obtém os poderes Curses e Cursed Power.',
+        rewards: {
+            general: [
+                { item: 'Avatar Soul', amount: 'x1', probability: 0.30 },
+                { item: 'Shadow Soul', amount: 'x1', probability: 0.20 },
+                { item: 'Obelisk Coin', amount: 'x1', probability: 0.0025 },
+                { item: 'Cursed Power Box', amount: 'x1', probability: 0.10 },
+                { item: 'Lunch Box', amount: 'x1', probability: 0.003 },
+                { item: 'Cursed Box', amount: 'x1', probability: 0.50 },
+                { item: 'Exp', amount: 'x1k', probability: 1 },
+                { item: 'Coins', amount: 'x100k', probability: 1 },
+            ],
+            lunchBox: [
+                { item: 'Chocolate Bar', amount: 'x1', probability: 0.11 },
+                { item: 'Cheese Pizza Slice', amount: 'x1', probability: 0.11 },
+                { item: 'Milk', amount: 'x1', probability: 0.11 },
+                { item: 'Green Gummy Bear', amount: 'x1', probability: 0.11 },
+                { item: 'Hot Sauce', amount: 'x1', probability: 0.12 },
+                { item: 'Coffee', amount: 'x1', probability: 0.11 },
+                { item: 'Doughnut', amount: 'x1', probability: 0.11 },
+                { item: 'Fried Egg', amount: 'x1', probability: 0.11 },
+                { item: 'Waffle', amount: 'x1', probability: 0.11 },
+            ],
+            cursedBox: [
+                { item: 'Dragon Race Token', amount: 'x8-13', probability: 0.10 },
+                { item: 'Saiyan Token', amount: 'x8-13', probability: 0.10 },
+                { item: 'Pirate Crew Token', amount: 'x8-13', probability: 0.10 },
+                { item: 'Demon Fruit Token', amount: 'x8-13', probability: 0.10 },
+                { item: 'Haki Token', amount: 'x8-13', probability: 0.10 },
+                { item: 'Reiatsu Token', amount: 'x8-13', probability: 0.10 },
+                { item: 'Zanpakuto Token', amount: 'x8-13', probability: 0.10 },
+                { item: 'Cursed Token', amount: 'x8-13', probability: 0.10 },
+                { item: 'Cursed Finger Token', amount: 'x8-13', probability: 0.10 },
+                { item: 'Sword Token', amount: 'x8-13', probability: 0.10 },
+            ],
+            cursedPowerBox: [
+                { item: 'Common Curse', probability: 0.4055 },
+                { item: 'Uncommon Curse', probability: 0.33 },
+                { item: 'Rare Curse', probability: 0.199 },
+                { item: 'Epic Curse', probability: 0.05 },
+                { item: 'Legendary Curse', probability: 0.01 },
+                { item: 'Mythical Curse', probability: 0.005 },
+                { item: 'Phantom Curse', probability: 0.0005 },
+            ]
+        }
+      }
   ],
   obelisks: [
     {
