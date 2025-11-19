b253cbe537ad8da148b56375c0b1d7bf363f7470
Alan Santos Coutinho <alanscoutinho0@gmail.com>
2025-11-19 21:30:28 +0000

    NPCs do mundo 8

--- src/data/worlds/world-8-data.js
+++ src/data/worlds/world-8-data.js
@@ -4,12 +4,12 @@ export const world8Data = {
   title: 'Mundo 8 - Ilha Shinobi',
   summary: 'Mundo ninja com dois chefes, Itechi e Madera, e foco em sorte e dano.',
   npcs: [
-    { name: 'Genin', rank: 'E', exp: 2800000, hp: '1SpD' },
-    { name: 'Chunin', rank: 'D', exp: 4000000, hp: '10SpD' },
-    { name: 'Jonin', rank: 'C', exp: 6000000, hp: '100SpD' },
-    { name: 'Anbu', rank: 'B', exp: 9000000, hp: '1OcD' },
-    { name: 'Kage', rank: 'A', exp: 13000000, hp: '10OcD' },
-    { name: 'Naruto', rank: 'S', exp: 18000000, hp: '1.41QnD' },
+    { name: 'Sekuri', rank: 'E', exp: 2800000, hp: '1SpD' },
+    { name: 'Kid Norto', rank: 'D', exp: 4000000, hp: '10SpD' },
+    { name: 'Kid Seske', rank: 'C', exp: 6000000, hp: '100SpD' },
+    { name: 'Kakashki', rank: 'B', exp: 9000000, hp: '1OcD' },
+    { name: 'Jiria', rank: 'A', exp: 13000000, hp: '10OcD' },
+    { name: 'Tsuni', rank: 'S', exp: 18000000, hp: '1.41QnD' },
     { name: 'Itechi', rank: 'SS', exp: 35000000, hp: '2.82QnD', drops: { aura: { name: 'Aura da Folha', probability: 0.05 } }, videoUrl: 'https://cdn.discordapp.com/attachments/1430337273794265250/1430338276535046155/ScreenRecording_10-21-2025_10-36-25_1.mov?ex=68fa1293&is=68f8c113&hm=429938bb3f8f6e74c5a5d5aa424f68a3eab8aeb7f3822abc25bc049bb5e058a7&' },
     { name: 'Madera', rank: 'SS', exp: 40000000, hp: '5.64QnD', drops: { aura: { name: 'Aura da Folha', probability: 0.05 } }, videoUrl: 'https://cdn.discordapp.com/attachments/1430337273794265250/1430338462162092093/ScreenRecording_10-21-2025_10-36-50_1.mov?ex=68fa12bf&is=68f8c13f&hm=6608bd1a66e65473faf5d3e05f279e8187d991512894aa533f07cf185ee2525e&' },
   ],
@@ -74,5 +74,3 @@ export const world8Data = {
     }
   ],
 };
-
-  
