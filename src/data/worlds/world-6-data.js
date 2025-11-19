a42263090a296366f2facff54181e082ec41bec4
Alan Santos Coutinho <alanscoutinho0@gmail.com>
2025-11-19 01:31:55 +0000

    Nome dos NPCs do mundo 6

--- src/data/worlds/world-6-data.js
+++ src/data/worlds/world-6-data.js
@@ -4,12 +4,12 @@ export const world6Data = {
   title: 'Mundo 6 - Ilha da Estátua',
   summary: 'Introduz o sistema de Shadows, que são lutadores especiais dropados por chefes.',
   npcs: [
-    { name: 'Cavaleiro de Pedra', rank: 'E', exp: 120000, hp: '1DD' },
-    { name: 'Gárgula', rank: 'D', exp: 180000, hp: '10DD' },
-    { name: 'Golem', rank: 'C', exp: 250000, hp: '100DD' },
-    { name: 'Guardião de Pedra', rank: 'B', exp: 350000, hp: '1tD' },
-    { name: 'Colosso de Pedra', rank: 'A', exp: 500000, hp: '10tD' },
-    { name: 'General de Pedra', rank: 'S', exp: 750000, hp: '97.5Ud' },
+    { name: 'Weak Sung', rank: 'E', exp: 120000, hp: '1DD' },
+    { name: 'Green Goblin', rank: 'D', exp: 180000, hp: '10DD' },
+    { name: 'White Tiger', rank: 'C', exp: 250000, hp: '100DD' },
+    { name: 'Cha', rank: 'B', exp: 350000, hp: '1tD' },
+    { name: 'Choi', rank: 'A', exp: 500000, hp: '10tD' },
+    { name: 'Solo Sung', rank: 'S', exp: 750000, hp: '97.5Ud' },
     { name: 'Statue of God', rank: 'SS', exp: 1500000, hp: '195Ud', drops: { aura: { name: 'Statue Aura', probability: 0.05 } }, videoUrl: 'https://cdn.discordapp.com/attachments/1430337273794265250/1430337947248361472/ScreenRecording_10-21-2025_10-32-26_1.mov?ex=68fa1245&is=68f8c0c5&hm=451a0c7979d34bf34b974d59f558aed19077082b2d29cf14342f0f5079111b12&' },
   ],
   powers: [
